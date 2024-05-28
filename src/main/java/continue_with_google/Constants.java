75a42db620fd4a7759797b6388d1d0ca6ace2c27
Nguyen Tan Dung <22520001@gm.uit.edu.vn>
2024-05-28 23:55:43 +0700

    TanDung

--- src/main/java/continue_with_google/Constants.java
+++
@@ -1,9 +0,0 @@
-//package continue_with_google;
-//public class Constants {
-//	 public static String GOOGLE_CLIENT_ID = "853526821776-n2qlqpahcjui51i13rug2h50vmahgheg.apps.googleusercontent.com";
-//	  public static String GOOGLE_CLIENT_SECRET = "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";
-//	  public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/QuanLyChiTieu/login_google";
-//	  public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
-//	  public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
-//	  public static String GOOGLE_GRANT_TYPE = "authorization_code";
-//}
